15c1e147120ba6cbce84c8fba5aed5d2031a8844
Justin Aybar <jaybar975@west-mec.org>
2019-02-21 19:43:21 -0700

    Final Commit Push- Ready for Review
    Added my first name, last name and date to the HTML document and the Javascript file. Linked the Javascript file to the HTML document. Added variables, functions and documents to the Javascript file

--- ph_clock.js
+++ ph_clock.js
@@ -1,20 +1,33 @@
+"use strict";
+
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Case Problem 4
 
    Countdown Clock for Ticket Ordering
-   Author: 
-   Date:  
+   Author: Justin Aybar
+   Date: 2.21.19 
 
-   Filename:   ph_clock.js     
+   Filename: ph_clock.js     
 
 */
 
+var minsLeft = 0;
+var secsLeft = 15;
+var clockID = setInterval("countDown()", 1000);
+var timeLeft = minsLeft * 60 + secsLeft;
 
-
-
-
+function countDown() {
+   minsLeft = Math.floor(timeLeft / 60);
+   secsLeft = timeLeft - minsLeft * 60;
+   var minsString = addLeadingZero(minsLeft);
+   var secsString = addLeadingZero(secsLeft);
+   document.getElementById("minutes").innerHTML = minsString;
+   document.getElementById("seconds").innerHTML = secsString;
+   checkTimer();
+   timeLeft--;
+}
 
 /* ------------------------------------------------- */
 
@@ -22,7 +35,7 @@
 /* The checkTimer() function tests whether there is any time left to make the
    ticket order. If the time left is 0, the stopClock() function is run;
    otherwise nothing happens and the program continues to run. */
-   
+
 function checkTimer() {
    if (timeLeft === 0) stopClock();
 }
